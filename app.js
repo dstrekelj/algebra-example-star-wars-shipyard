446a7ce084af0713f821f6a3cb0b53483c2ecff4
Domagoj Štrekelj <domagojstrekelj@gmail.com>
2022-03-04 20:48:47 +0100

    Add handlebars template

--- app.js
+++ app.js
@@ -2,14 +2,13 @@ const appElement = document.querySelector("#app");
 const source = document.querySelector("#starships-template").innerHTML;
 const template = Handlebars.compile(source);
 
-/*
 const endpoint = "https://swapi.dev/api/starships";
 
 fetch(endpoint)
   .then((response) => response.json())
-  .then((json) => console.log(json))
-  .catch((error) => console.error(error));
-*/
+  .then((json) => {
+    const html = template({ starships: json.results });
 
-const html = template({ starships: [{ name: "Ship A1" }, { name: "Ship B1" }] });
-appElement.innerHTML = html;
+    appElement.innerHTML = html;
+  })
+  .catch((error) => console.error(error));
